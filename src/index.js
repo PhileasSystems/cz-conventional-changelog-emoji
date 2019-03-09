fc47ae35134b3ffe1fc4cd68b92ad34bc84193d4
Adnene KHALFA <adnene.khalfa@gmail.com>
2019-03-09 18:53:03 +0100

    feat(emojis): add new commit types
    review, upgrade, downgrade, deployment, hotfix and security

--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types-emoji');
 
 module.exports = engine({
-  types: conventionalCommitTypes.types
+  types: {
+    ...conventionalCommitTypes.types,
+    review: {
+      "description": "👌  Code changed after code review"
+    },
+    upgrade: {
+      "description": "⬆️  Upgrade package.json dependency"
+    },
+    downgrade: {
+      "description": "⬇️  Upgrade package.json dependency"
+    },
+    deployment: {
+      "description": "🚀  Deploying stuff to remote environment"
+    },
+    hotfix: {
+      "description": "🚑  Critical hotfix"
+    },
+    security: {
+      "description": "🔒  Changes affecting security, access, ACLs..."
+    }
+  }
 });
